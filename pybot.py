a373a0828166b5c70aa5181da6479f488b60bfda
Soumik Ghosh <soumik.ghosh1998@gmail.com>
2020-02-23 15:29:40 +0530

    paused ;say funtion
    limiter to be added to the ;say funtion

--- pybot.py
+++ pybot.py
@@ -100,11 +100,11 @@ async def clear_error(ctx, error):
 
 
 
-@client.command(aliases=['Say','say'])
-async def tts(ctx, *, message: str):
-	await ctx.send(message, tts=True)
-	#await ctx.send("Anindya ei function ta bondho korte boleche :'(\nNa korle bot kick kore debe")	      
-	print('TTS function used')
+# @client.command(aliases=['Say','say'])
+# async def tts(ctx, *, message: str):
+# 	await ctx.send(message, tts=True)
+# 	#await ctx.send("Anindya ei function ta bondho korte boleche :'(\nNa korle bot kick kore debe")	      
+# 	print('TTS function used')
 
 
 
